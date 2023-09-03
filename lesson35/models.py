69fb753e2b8caa74a429335d370bc62ef9511374
olex <olexandr.samus.2004@gmail.com>
2023-09-03 09:53:25 +0300

    add lesson35

---
+++ lesson35/models.py
@@ -0,0 +1,13 @@
+# SQL
+
+# Create table names
+# Alter - зміна об'єкту
+# Drop - видаляє об'єкт
+
+
+# Select names
+# Insert - додає нові дані
+# Update - змінює існуючі дані
+# Delete - видаляє
+
+# Er-модель
